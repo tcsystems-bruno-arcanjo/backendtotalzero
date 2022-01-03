df7b5329db04045012847bd44bc17204c351920a
Bruno Arcanjo <barcanjo@thecodes.io>
2022-01-03 20:11:54 -0300

    chore: altera as configurações de conexão com o banco de dados para incluir variáveis do ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/backendtotalzero'),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USERNAME || 'mongo'}:${process.env.DATABASE_PASSWORD || ''}@${process.env.DATABASE_SERVER || 'localhost'}/${process.env.DATABASE_NAME || 'backendtotalzero'}?retryWrites=true&w=majority`),
     UsersModule
   ],
 })
