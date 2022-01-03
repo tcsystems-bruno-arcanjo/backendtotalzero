c3ea425400ba81f662267ba8d9e0408dc1abae96
Bruno Arcanjo <barcanjo@thecodes.io>
2022-01-03 20:50:54 -0300

    chore: adiciona a configuração de host para iniciar a aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || '0.0.0.0';
+  await app.listen(port, host);
 }
 bootstrap();
